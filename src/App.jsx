4b71aef0260a8b6f15e46b632a6b919cd28c9663
Arithz <harithzfathi2001@gmail.com>
2023-11-30 23:49:49 +0800

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
@@ -10,15 +10,15 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App" style = {appStyle}>
-      <Navbar/>
-      <Hero/>
-      <About/>
-      <Team/>
-      <Services/>
-      <Gallery/>
-      <Testimonial/>
-      <Footer/>
+    <div className="App" style={appStyle}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Team />
+      <Services />
+      <Gallery />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
@@ -33,9 +33,10 @@ const appStyle = {
   padding: "0",
   textAlign: "center",
   width: "100%",
-  height:"100%",
-  display:"flex",
+  height: "100%",
+  display: "flex",
   flexDirection: "column",
+  gap: "0.5rem",
 };
 
 export default App;
