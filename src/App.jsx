56e69cd80ba790e6170b0042b38f033b841411c0
Harithz <67273525+Arithz@users.noreply.github.com>
2023-11-30 02:51:01 +0000

    initial layout for sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
 import './App.css';
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Team from "./components/Team";
+import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+import Gallery from "./components/Gallery";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div className="App" style = {appStyle}>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Team/>
+      <Services/>
+      <Gallery/>
+      <Testimonial/>
+      <Footer/>
     </div>
   );
 }
+
+const appStyle = {
+  backgroundColor: "#f5f5f5",
+  color: "#000",
+  fontFamily: "sans-serif",
+  fontSize: "16px",
+  lineHeight: "1.5",
+  margin: "0",
+  padding: "0",
+  textAlign: "center",
+  width: "100%",
+  height:"100%",
+  display:"flex",
+  flexDirection: "column",
+};
 
 export default App;
