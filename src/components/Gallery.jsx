99f6bdb94ef547dd741ce52efe0d0de72c42ad3b
jiersss <84037089+jaaart15@users.noreply.github.com>
2023-12-30 16:21:36 +0800

    update gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,3 +1,34 @@
-export default function Gallery() {
-  return <div id="gallery">Gallery</div>;
-}
+import React from 'react';
+
+const Gallery = () => {
+  // Array of image sources
+  const images = [
+    '/img/service.webp',
+    '/img/service.webp',
+    '/img/service.webp',
+    '/img/service.webp',
+    '/img/service.webp',
+    '/img/service.webp',
+
+  ];
+
+  return (
+    <div id='gallery'>
+    <div className='gallery_section'>
+      <h1 className='gallery_text'>My Gallery</h1>
+      <div className='gallery_grid'>
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Image ${index + 1}`}
+            className='gallery_image'
+          />
+        ))}
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Gallery;
