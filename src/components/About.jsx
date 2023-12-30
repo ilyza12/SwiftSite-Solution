f9aa8013aaf7f5c59cd910d1a2e0a59de77d1ae1
a1man <danishaiman3b@gmail.com>
2023-12-30 17:35:35 +0800

    About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,20 @@
-export default function About() {
-  return <div id="about">About</div>;
+import React from "react";
+
+function About() {
+  return (
+    <section className="about-white">
+      <div className="about">
+        <h2 className="about-title">About Us</h2>
+        <p className="about-subtitle">
+          We are a team of passionate individuals dedicated to creating the best possible product for our customers. With years of experience in our respective fields, we bring a wealth of knowledge and expertise to every project we undertake.
+        </p>
+        <p className="about-subtitle">
+          Our mission is to provide high-quality services that exceed our customers' expectations, and we strive to continually improve our processes and offerings to meet and exceed those needs.
+        </p>
+      </div>
+    </section>
+  );
 }
+
+export default About;
+
