5eef4f87764412b40e917668947b1ae4d128676a
a1man <danishaiman3b@gmail.com>
2023-12-31 17:27:37 +0800

    Update About & Team

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,11 +10,49 @@ function About() {
           for our customers. With years of experience in our respective fields, we bring a wealth of
           knowledge and expertise to every project we undertake.
         </p>
-        <p className="about-subtitle">
-          Our mission is to provide high-quality services that exceed our customers' expectations,
-          and we strive to continually improve our processes and offerings to meet and exceed those
-          needs.
-        </p>
+        <div className="about-mission">
+          <img src="/img/mission.png" alt="Mission" className="about-icon mission-icon" />
+          <div>
+            <h3 className="about-title-mission">Mission</h3>
+            <p className="about-subtitle-mission">
+              Our mission is to provide high-quality services that exceed our customers' expectations,
+              and we strive to continually improve our processes and offerings to meet and exceed those
+              needs.
+            </p>
+          </div>
+        </div>
+        <div className="about-vision">
+          <img src="/img/vision.png" alt="Vision" className="about-icon vision-icon" />
+          <div>
+            <h3 className="about-title-vision">Vision</h3>
+            <p className="about-subtitle-vision">
+              Our vision is to be a global leader in XYZ services, known for exceptional customer service,
+              innovative solutions, and a strong commitment to sustainability and positive societal impact.
+            </p>
+          </div>
+        </div>
+        <div className="about-values">
+          <img src="/img/values.png" alt="Values" className="about-icon values-icon" />
+          <div>
+            <h3 className="about-title-values">Values</h3>
+            <ul className="about-subtitle-values">
+              <li>Integrity and trust guide every interaction.</li>
+              <li>Dedication to continuous improvement and growth.</li>
+              <li>Innovation drives us to deliver cutting-edge solutions.</li>
+              <li>Collaboration is our strength for achieving excellence.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="about-video">
+          <iframe
+            title="Corporate Video"
+            src="https://www.youtube.com/embed/iyd7qUH3oH0?si=Fx4LrtfagFZFP_bh"
+            width="760" 
+            height="415"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </div>
     </section>
   );
